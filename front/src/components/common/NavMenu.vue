5913d9fa89803dc9d6dbbdadc13c1bce89acef39
lzrfight <1280565957@qq.com>
2020-04-15 02:17:37 +0800

    完成后台

--- front/src/components/common/NavMenu.vue
+++ front/src/components/common/NavMenu.vue
@@ -10,8 +10,8 @@
     <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name">
       {{ item.navItem }}
     </el-menu-item>
-    <a href="/login" style="color: #222;float: right;padding: 20px;">登陆</a>
-    <i class="el-icon-menu" style="float:right;font-size: 45px;color: #222;padding-top: 8px"></i>
+    <a href="/login" style="color: #222;float: right;padding: 20px;">登陆/注册</a>
+    <i class="el-icon-switch-button" v-on:click="logout" style="font-size: 40px;float: right"></i>
     <span style="position: absolute;padding-top: 20px;right: 43%;font-size: 20px;font-weight: bold">卓然米粉</span>
   </el-menu>
 </template>
@@ -21,15 +21,40 @@
     name: 'NavMenu',
     data() {
       return {
+         username: this.$store.state.user.username,
         navList: [
           {name: '/index', navItem: '首页'},
-          {name: '/order', navItem: '点餐'},
+          {name: '/food', navItem: '点餐'},
           {name: '/discount', navItem: '促销'},
           {name: '/restaurant', navItem: '餐厅'},
           {name: '/about', navItem: '关于我们'}
         ]
       }
-    }
+    },
+    // mounted: function () {
+    //   this.loadUser()
+    // },
+    methods:
+      {
+        logout(){
+          const _this = this;
+          this.$axios.get('/logout').then(resp =>
+          {
+            if (resp && resp.data.code === 200)
+            {
+              _this.$store.commit('logout');
+              _this.$router.replace('/index');
+              this.  username = "登陆注册"
+              this.$alert('退出成功','提示',{
+                confirmButtonText:'确定'
+              })
+            }
+          }).catch(failResponese => {this.$alert('退出没反应都操了','提示',{
+            confirmButtonText:'确定'
+          })})
+        },
+      },
+
   }
 </script>
 
@@ -40,6 +65,10 @@
 
   span {
     pointer-events: none;
+  }
+  .el-icon-switch-button {
+    cursor: pointer;
+    outline:0;
   }
 </style>
 
